a9addff9a6aa8184ff31fb2a90b4c776db6bd4be
Luis Felipe Patrocinio <patrocinioluisf@gmail.com>
2025-09-10 10:56:55 -0300

    fix: Add missing apIP variable declaration
    Added missing declaration of apIP variable in network_setup.h to resolve compilation errors.

--- src/network_setup.h
+++ src/network_setup.h
@@ -1,6 +1,8 @@
 #ifndef NETWORK_SETUP_H
 #define NETWORK_SETUP_H
 
+extern const IPAddress apIP;
+
 // Declare the setup functions that main.cpp will call
 void setupNetworkServices();
 void startDnsTask();
